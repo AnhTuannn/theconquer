df8c33bcb66e910607e048a29277ee895bffd79c
AnhTuannn <100067409+AnhTuannn@users.noreply.github.com>
2022-07-29 00:48:36 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,32 @@
+window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+    if (currentScrollPos > 20) {
+      document.getElementById("nav-scroll").style.display = "none";
+    } else {
+      document.getElementById("nav-scroll").style.display = "initial";
+    }
+  }
+
+  const validateEmail = (email) => {
+    return email.match(
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+  };
+  
+  const validate = () => {
+    const $result = $('#result');
+    const email = $('#email').val();
+    $result.text('');
+  
+    if (validateEmail(email)) {
+      $result.text(email + ' is valid :)');
+      $result.css('color', 'green');
+    } else {
+      $result.text(email + ' is not valid :(');
+      $result.css('color', 'red');
+    }
+    return false;
+  }
+  
+  $('#email').on('input', validate);
+
